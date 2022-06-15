8324856e37a523742ead39db053cfd67e7efb03c
syaiful <syaifulmaulana817@gmail.com>
2022-06-15 20:49:22 +0700

    middlewares auth

--- routes/product/index.js
+++ routes/product/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const product = require('../../controllers/product/indexController');
+const { login } = require('../../middlewares/auth');
 // welcome
-routes.post('/add', product.add);
+routes.post('/add', login, product.add);
 
 module.exports = routes;
